3f6bece25e8d0b40460a0fe8b5dfe6839d08ea77
halixaoqiu <linqiu.back@gmail.com>
2014-06-19 23:29:44 +0800

    add schema.sql

---
+++ schema.sql
@@ -0,0 +1,48 @@
+
+-- ----------------------------
+-- Create database bookmark
+-- ----------------------------
+create database bookmark;
+
+-- ----------------------------
+-- Table structure for `bookmark`
+-- ----------------------------
+-- DROP TABLE IF EXISTS bookmark;
+CREATE TABLE bookmark (
+    bookmark_id BIGINT AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(100) NOT NULL,
+    summary TEXT,
+    classify VARCHAR(100),
+    tag VARCHAR(100),
+    is_public TINYINT(1) DEFAULT 1,
+    create_time DATETIME NOT NULL DEFAULT NOW(),
+    modify_time DATETIME NOT NULL DEFAULT NOW(),
+    primary key (bookmark_id)
+);
+alter table bookmark AUTO_INCREMENT=1025;
+
+insert into bookmark (user_id,title,url,summary,classify,tag,is_public) values ('123456','知乎','http://www.zhihu.com/',
+'一个社会化的知识分享网站','收藏备忘','知乎，quora','1');
+
+-- ----------------------------
+-- Table structure for `admin`
+-- If u need u can add a separate table for admin
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+CREATE TABLE user(
+	user_id BIGINT AUTO_INCREMENT,
+	nick VARCHAR(50) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	email VARCHAR(100),
+	privilege TINYINT(5) DEFAULT 0,
+	type TINYINT(5) DEFAULT 0,
+	create_time DATETIME NOT NULL DEFAULT NOW(),
+    modify_time DATETIME NOT NULL DEFAULT NOW(),
+    primary key (user_id)
+);
+alter table user AUTO_INCREMENT=2045;
