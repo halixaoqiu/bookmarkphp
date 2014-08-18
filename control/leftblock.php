45b587b7c25cc78deb8f2ef874f2cca16de4339c
halixaoqiu <linqiu.back@gmail.com>
2014-08-18 23:37:44 +0800

    add count code

--- control/leftblock.php
+++ control/leftblock.php
@@ -25,9 +25,29 @@
 		array_push($tag_merge_array, $arr);
 	}
 	
-	$tag_html = "<ul><li class='side-nav-li'><a class='side-nav-link' href=\"tag.php?tag=&type=all\">所有收藏</a></li>";
-	$tag_html = $tag_html."<li class='side-nav-li'><a class='side-nav-link' href=\"tag.php?tag=&type=public\">公开收藏</a></li>";
-	$tag_html = $tag_html."<li class='side-nav-li'><a class='side-nav-link' href=\"tag.php?tag=&type=private\">私有收藏</a></li>";
+	//count当前用户所有收藏
+	$all_count = 0;
+	$stmt = $pdo->prepare("select count(bookmark_id) as count from bookmark where user_id=?");
+	$stmt->execute(array($user_id));
+	if($stmt->rowCount()>0){
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$all_count = $row['count'];
+	}
+	
+	//count当前用户公开收藏
+	$public_count = 0;
+	$stmt = $pdo->prepare("select count(bookmark_id) as count from bookmark where user_id=? and is_public=1");
+	$stmt->execute(array($user_id));
+	if($stmt->rowCount()>0){
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$public_count = $row['count'];
+	}
+	
+	//count当前用户私有收藏
+	$private_count = $all_count - $public_count;
+	
+	$tag_html = "<ul><li class='side-nav-li'><a class='side-nav-link' href=\"tag.php?tag=&type=public\">公开收藏<span class='my-tag-count'>".$public_count."</span></a></li>";
+	$tag_html = $tag_html."<li class='side-nav-li'><a class='side-nav-link' href=\"tag.php?tag=&type=private\">私有收藏<span class='my-tag-count'>".$private_count."</span></a></li>";
 	foreach($tag_merge_array as $tag){
 		$tag_html = $tag_html."<li class='side-nav-li'><a class='side-nav-link' href=\"tag.php?tag=".$tag['tag_name']."\">".$tag['tag_name']."<span class='my-tag-count'>".$tag['tag_count']."</span></a></li>";
 	}
@@ -43,7 +63,7 @@
 		</div>
 	</div>
 	<div class="my-tags">
-		<span><h3>我的收藏</h3></span>
+		<span><h3><a class='side-my-bookmarks' href="tag.php?tag=&type=all">我的收藏<span class='my-tag-count'><?php echo $all_count?></span></a></h3></span>
 		<?php echo $tag_html ?>
 	</div>
 </div>
